688a1d8e02f4f7a0e3d58401f4c6eead7ae14f61
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2021-10-18 12:24:41 +0300

    undo https

--- WebCellLifeSimulationApp/Simulation.cs
+++ WebCellLifeSimulationApp/Simulation.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Timers;
 using System.Threading.Tasks;
+using System.Timers;
 using SimulationModelCLR;
 
 namespace WebCellLifeSimulationApp
