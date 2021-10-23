b04227f3012c1020afcec8bc235b185154ad3a45
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2021-10-23 06:40:06 +0300

    style fix

--- WebCellLifeSimulationApp/ClientApp/src/app/home/home.component.ts
+++ WebCellLifeSimulationApp/ClientApp/src/app/home/home.component.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements AfterViewInit {
   @ViewChild('myCanvas', { static: false }) myCanvas: ElementRef;
