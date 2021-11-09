bc8f72a727ea6e8184d19020767c167044c25343
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2021-11-09 23:18:05 +0300

    app style

--- QtCellLifeSimulationApp/main.cpp
+++ QtCellLifeSimulationApp/main.cpp
@@ -1,10 +1,15 @@
 #include "MainWindowCLS.h"
 #include <QtWidgets/QApplication>
+#include <QStyleFactory>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    QApplication a(argc, argv);
-    MainWindowCLS w;
-    w.show();
-    return a.exec();
+	QCoreApplication::setApplicationName(QStringLiteral("C3DShellCodingTutorial"));
+	QCoreApplication::setOrganizationName(QStringLiteral("Maxim Chistyakov"));
+	QCoreApplication::setOrganizationDomain(QStringLiteral("https://github.com/maxchistt/CellLifeSimulation"));
+	QApplication::setStyle(QStyleFactory::create("Fusion"));
+	QApplication a(argc, argv);
+	MainWindowCLS w;
+	w.show();
+	return a.exec();
 }
