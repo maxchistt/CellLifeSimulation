81d93ec4fec7cd98ea8973efdb0da6cc01186134
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2021-11-11 22:42:32 +0300

    preserve c3d usage

--- QtCellLifeSimulationApp/C3D_USAGE.h
+++ QtCellLifeSimulationApp/C3D_USAGE.h
@@ -1,5 +1,9 @@
 #pragma once
-#define C3D_USAGE
-#define LKEY ""
-#define LSIGN ""
+// You need to copy Kernel and Vision distributions into C3D folder at first
+// Define C3D_USAGE to use C3D in project
+// Define actual licence key and licence signature
+
+//#define C3D_USAGE
+#define LKEY "qwerty.12345.[WIN][cnv][mdl][slv][vsn][bsh]"
+#define LSIGN "asdfghjkl=="
 
